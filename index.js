4c950163b20afe43376218978f7747a9e8f0f254
Will Scott <will@cypherpunk.email>
2019-08-05 18:54:51 -0700

    initial bot

---
+++ index.js
@@ -0,0 +1,13 @@
+const telegraf = require('telegraf')
+const config = require('./config')
+
+const bot = new telegraf(config.token)
+bot.start((ctx) => ctx.reply('您好，請加我在您的群子裡'))
+bot.on('message', async (ctx) => {
+    if (ctx.updateSubTypes.indexOf('document') > -1 ) {
+        await ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id)
+    }
+    console.log(ctx.updateSubTypes)
+    console.log(ctx.message)
+})
+bot.launch()
